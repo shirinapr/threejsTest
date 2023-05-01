9558f2cb6f0e616f912ada05cce4a55dd3999f5b
shirinapr <shirinapr1@gmail.com>
2023-05-01 13:27:47 +0430

    fix: problem with build.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import './App.css';
-import * as THREE from 'three';
-
 function App() {
-  const scene = new THREE.Scene();
-
-  const geometry = new THREE.SphereGeometry(3, 64, 64);
-
-  const material = new THREE.MeshStandardMaterial({
-    color: 'pink',
-  });
-
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  const camera = new THREE.PerspectiveCamera(45, 800, 600);
-  scene.add(camera);
-
-  const canvas = document.querySelector('.webgl');
-  const render = new THREE.WebGL1Renderer({ canvas });
-
   return (
     <>
-      <canvas class="webgl"></canvas>
+      <canvas id="webgl"></canvas>
       <p>hey</p>
     </>
   );
